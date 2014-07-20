704d41f2ecec28786e8562614668c4e936868987
Rajeev Vij <rvij@cisco.com>
2014-07-20 10:14:12 -0700

    third commit

--- rvijpracML.R
+++ rvijpracML.R
@@ -6,23 +6,21 @@ dataset  <- read.csv("../pml-training.csv", na.strings=c("NA",""), strip.white=T
 dim(dataset)
 dataset_test <- read.csv("../pml-testing.csv", na.strings=c("NA",""), strip.white=T)
 
-createTidyDataset = function(data){
-  # Cleaning the data:
-  isNAcolumns <- apply(data, 2, function(x) { sum(is.na(x)) })
-  
-  tidyData <- subset(data[, which(isNAcolumns == 0)], 
-         select=-c(X, user_name, new_window, num_window, 
-                   raw_timestamp_part_1, raw_timestamp_part_2, 
-                   cvtd_timestamp))
-}
-
-
 # Check the summary of the data frame.
 summary(dataset)
 
 # There are large number of attributes that have NAs, we will ignore those variable as they may not contribute to 
 # any predictive capability, also there are some time variables that will not be used
 
+createTidyDataset = function(data){
+  # Cleaning the data:
+  isNAcolumns <- apply(data, 2, function(x) { sum(is.na(x)) })
+  
+  tidyData <- subset(data[, which(isNAcolumns == 0)], 
+                     select=-c(X, user_name, new_window, num_window, 
+                               raw_timestamp_part_1, raw_timestamp_part_2, 
+                               cvtd_timestamp))
+}
 
 dataset <- createTidyDataset(dataset)
   
